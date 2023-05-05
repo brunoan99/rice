cb4d817ec15f15089f915792cf9f11c889f303a9
Bruno Andrade <brunoan99@gmail.com>
2023-05-05 03:42:22 -0300

    rofi: add rofi to rice

---
+++ rofi/install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Install
+
+sudo pacman -S rofi rofi-emoji
---
+++ rofi/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir -p $HOME/.config/rofi
+ln -sf $HOME/.rice/rofi/config.rasi $HOME/.config/rofi/config.rasi
+ln -sf $HOME/.rice/rofi/power.rasi $HOME/.config/rofi/power.rasi
