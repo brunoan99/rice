289ff5a0a8a9bec23a4b7a2893178558a5be048b
Bruno Andrade <brunoan99@gmail.com>
2023-05-05 04:34:09 -0300

    polybar: add session module using rofi and change visual configs

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,8 +6,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar left 2>&1 | tee -a /tmp/polybar.left.log & disown
-polybar right 2>&1 | tee -a /tmp/polybar.right.log & disown
-
-echo "Polybar launched..."
+polybar left &
+polybar right &
