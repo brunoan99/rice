18690e6797b531ba8bdf0e85b15b97a52149f587
Bruno Andrade <brunoan99@gmail.com>
2023-05-07 03:46:11 -0300

    scripts: add setup to shell scripts in local

---
+++ .local/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# setup the scripts in /bin
+
+mkdir -p $HOME/.local/bin
+
+ln -sf $HOME/.rice/.local/bin/autostart $HOME/.local/bin/autostart
+ln -sf $HOME/.rice/.local/bin/powermenu $HOME/.local/bin/powermenu
+
