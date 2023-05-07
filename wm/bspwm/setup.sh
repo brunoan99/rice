686870fcbf00baa788325e9356897fd65b120e4e
Bruno Andrade <brunoan99@gmail.com>
2023-05-07 03:53:34 -0300

    bspwm: improve descriptions in setup file

--- wm/bspwm/setup.sh
+++ wm/bspwm/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+# create folders to bspwm and sxhkd config
 mkdir -p $HOME/.config/bspwm
-ln -sf $HOME/.rice/wm/bspwm/bspwmrc $HOME/.config/bspwm/bspwmrc
 mkdir -p $HOME/.config/sxhkd
+
+# link the files to the specific config folder
+ln -sf $HOME/.rice/wm/bspwm/bspwmrc $HOME/.config/bspwm/bspwmrc
 ln -sf $HOME/.rice/wm/bspwm/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
