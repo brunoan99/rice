2250bc07b5a13178e499b262297c3c2ed6732dfb
Bruno Andrade <brunoan99@gmail.com>
2023-05-07 03:45:44 -0300

    sddm: update scripts

--- sddm/install.sh
+++ sddm/install.sh
@@ -2,5 +2,5 @@
 # install sddm, enable sddm in systemctl and install Sugar-Candy theme
 
 sudo pacman -S sddm
-systemctl enable sddm
+sudo systemctl enable sddm
 yay -S sddm-theme-sugar-candy-git
--- sddm/setup.sh
+++ sddm/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Setup the files configs
 
-cp /home/snape/.rice/sddm/Xsetup /usr/share/sddm/scripts/Xsetup
-cp /home/snape/.rice/sddm/sddm.conf /etc/sddm.conf
-cp /home/snape/.rice/sddm/theme.conf /usr/share/sddm/themes/Sugar-Candy/theme.conf
+cp $HOME/.rice/sddm/Xsetup /usr/share/sddm/scripts/Xsetup
+cp $HOME/.rice/sddm/sddm.conf /etc/sddm.conf
+cp $HOME/.rice/sddm/theme.conf /usr/share/sddm/themes/Sugar-Candy/theme.conf
