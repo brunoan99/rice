56aedf8a0804aea8b4e68f0e2f7a21b8537695d6
Bruno Andrade <brunoan99@gmail.com>
2023-11-17 13:44:16 -0300

    feat(keyring): add keyring and scripts

---
+++ gnome-keyring/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Install picom
+
+yay -S gnome-keyring
+sudo pacman -S picom
+yay -S seahorse
---
+++ gnome-keyring/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Setup the config file
+
+sudo ln -sf $HOME/.rice/gnome-keyring/login /etc/pam.d/login
